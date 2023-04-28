d3e75292a9eaf8536eabc920f874385eddf2bd60
dhanushtheijas <111133555+dhanushtheijas08@users.noreply.github.com>
2023-04-28 12:05:28 +0530

    Update Btn.js

--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -3,7 +3,7 @@ import React from "react";
 const Btn = ({ name, togglePopUp }) => (
   <button
     className="bg-btn-color text-whitePrime py-2 px-4 text-2xl rounded-md"
-    onClick={togglePopUp}
+    onClick={ togglePopUp }
   >
     {name}
   </button>
