f3b9d63089ce4251e868d535c094fe07bfd4ad0b
dhanushtheijas <111133555+dhanushtheijas08@users.noreply.github.com>
2023-05-03 16:52:46 +0530

    Update Heading.js

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -1,6 +1,6 @@
 let Heading = function ({ title,size }) {
   let style = `text-darkGray ${size} font-semibold max-w-fit mx-auto`;
-  return <h1 className={style}>{title}</h1>;
+  return <h1 className={style}>{title} </h1>;
 };
 
 export default Heading;
