136b138ffbb4bfaa50d6dcf7428ee74fe0575272
dhanushtheijas <111133555+dhanushtheijas08@users.noreply.github.com>
2023-05-18 07:41:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App className="relative" />
+    <App className="relative"/>
   </React.StrictMode>
 );
 
